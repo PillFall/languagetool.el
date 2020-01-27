daba1a34794a64c8e7577cb487370488ec88acc2
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2020-01-27 16:48:05 -0500

    Add URL to header

--- languagetool.el
+++ languagetool.el
@@ -4,6 +4,7 @@
 
 ;; Author: Joar Buitrago <jebuitragoc@unal.edu.co>
 ;; Keywords: grammar text docs tools
+;; URL: https://github.com/PillFall/Emacs-LanguageTool.el
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1"))
 
