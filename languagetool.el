604d3eed84d1ed0a58ebf0b8844a1a02c1d7d34c
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2020-11-11 00:01:22 -0500

    Drop LanguageTool Server file

--- languagetool.el
+++ languagetool.el
@@ -31,6 +31,9 @@
 (eval-and-compile (require 'languagetool-issue-faces))
 
 (require 'json)
+(require 'request)
+
+
 
 (defgroup languagetool nil
   "LanguageTool's customization group."
@@ -57,6 +60,10 @@ recommends to use:
   :type '(choice
           (list string)))
 
+
+
+;; Variables related to LanguageTool Command Line:
+
 (defcustom languagetool-language-tool-jar nil
   "Absolute path to LanguageTool command line jar file."
   :group 'languagetool
@@ -69,6 +76,69 @@ More info at http://wiki.languagetool.org/command-line-options"
   :group 'languagetool
   :type '(choice
           (list string)))
+
+(defvar languagetool-output-buffer-name "*LanguageTool Output*")
+
+(defcustom languagetool-error-exists-hook nil
+  "Hook run after LanguageTool process found any error(s)."
+  :group 'languagetool
+  :type 'hook)
+
+(defcustom languagetool-no-error-hook nil
+  "Hook run after LanguageTool report no error."
+  :group 'languagetool
+  :type 'hook)
+
+(defcustom languagetool-finish-hook nil
+  "Hook run after cleanup buffer."
+  :group 'languagetool
+  :type 'hook)
+
+
+
+;; Variables related to LanguageTool Server:
+
+(defcustom languagetool-server-language-tool-jar nil
+  "Absolute path to LanguageTool server jar file."
+  :group 'languagetool
+  :type 'file)
+
+(defcustom languagetool-server-url "http://localhost"
+  "URL to be used to communicate to LanguageTool Server."
+  :group 'languagetool
+  :type 'string)
+
+(defcustom languagetool-server-port 8081
+  "Port to be used to communicate to LanguageTool Server."
+  :group 'languagetool
+  :type 'integer)
+
+(defcustom languagetool-server-delayed-commands
+  '(after-save-hook)
+  "List of the commands that are searched for sending and parsing correction."
+  :group 'languagetool
+  :type '(repeat (symbol)))
+
+(defvar languagetool-server-output-buffer-name "*LanguageTool Server Output*")
+
+(defvar languagetool-server-process nil
+  "A reference to the LanguageTool Server executable.")
+
+(defvar languagetool-server--start-check-timer nil
+  "Hold idle timer watch every LanguageTool processed buffer.")
+
+(defvar languagetool-server--start-check-delay 0.2
+  "Number of seconds while idle to wait before checking again for initialized server.")
+
+(defvar languagetool-server--started-p nil
+  "Tell if the server can be used or not.")
+
+(defvar languagetool-server--correcting-p nil
+  "Tell if we are actually correcting the buffer")
+
+
+
+;; Shared variables:
 
 (defcustom languagetool-default-language "auto"
   "Language name which LanguageTool will set for correction.
@@ -90,21 +160,6 @@ List of strings."
   :type '(choice
           (list string)))
 
-(defcustom languagetool-error-exists-hook nil
-  "Hook run after LanguageTool process found any error(s)."
-  :group 'languagetool
-  :type 'hook)
-
-(defcustom languagetool-no-error-hook nil
-  "Hook run after LanguageTool report no error."
-  :group 'languagetool
-  :type 'hook)
-
-(defcustom languagetool-finish-hook nil
-  "Hook run after cleanup buffer."
-  :group 'languagetool
-  :type 'hook)
-
 (defvar languagetool--correction-keys
   [?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9 ?0
       ;; suggestions may over 10.
@@ -112,8 +167,6 @@ List of strings."
       ?a ?b ?c ?d ?e ?f ?g ?h ?i ?j
       ?k ?l ?m ?n ?o ?p ?q ?r ?s ?t
       ?u ?v ?w ?x ?y ?z])
-
-(defvar languagetool-output-buffer-name "*LanguageTool Output*")
 
 (defcustom languagetool-hint-function
   'languagetool-hint-default-function
@@ -153,7 +206,7 @@ A example hint function:
 
 
 
-;; Local Variables:
+;; Buffer Local Variables:
 
 (defvar languagetool-local-disabled-rules nil
   "Disabled rules pass to LanguageTool.  Buffer local.
@@ -166,6 +219,197 @@ List of strings.")
 
 
 ;; Functions:
+
+;; Start Server Functions:
+
+;;;###autoload
+(define-minor-mode languagetool-server-mode
+  "Minor mode that highlights LanguageTool corrections."
+  :group 'languagetool
+  :lighter " LT"
+  (languagetool-server--toggle))
+
+;;;###autoload
+(defun languagetool-server-start ()
+  "Start the LanguageTool Server executable.
+
+Its not recommended to run this function more than once."
+  (interactive)
+  (unless (process-live-p languagetool-server-process)
+    (unless (executable-find languagetool-java-bin)
+      (error "Java could not be found"))
+    (unless languagetool-server-language-tool-jar
+      (error "LanguageTool Server jar path is not set"))
+    (unless (file-readable-p languagetool-server-language-tool-jar)
+      (error "LanguageTool Server jar is not readable or could not be found"))
+
+    (let ((buffer (get-buffer-create languagetool-server-output-buffer-name)))
+      (with-current-buffer buffer
+        (erase-buffer))
+      (setq languagetool-server-process
+            (start-process "*LanguageTool Server*"
+                           buffer
+                           languagetool-java-bin
+                           "-cp"
+                           languagetool-server-language-tool-jar
+                           "org.languagetool.server.HTTPServer"
+                           "--port"
+                           (format "%d" languagetool-server-port)))
+      (set-process-query-on-exit-flag languagetool-server-process nil))
+
+    (setq languagetool-hint--timer
+          (run-with-idle-timer languagetool-hint-idle-delay t
+                               languagetool-hint-function))))
+
+;;;###autoload
+(defun languagetool-server-stop ()
+  "Stops the LanguageTool Server executable."
+  (interactive)
+  (setq languagetool-server--started-p nil)
+  (delete-process languagetool-server-process)
+  (when languagetool-hint--timer
+    (cancel-timer languagetool-hint--timer))
+
+  ;; Delete active server checking timer if still active
+  (when languagetool-server--start-check-timer
+    (cancel-timer languagetool-server--start-check-timer)))
+
+(defun languagetool-server--toggle ()
+  "Start or closes LanguageTool Server."
+  (if languagetool-server-mode
+      (progn
+        ;; Start checking for LanguageTool server open
+        (setq languagetool-server--start-check-timer
+              (run-with-timer 0 languagetool-server--start-check-delay
+                              #'languagetool-server--start-check))
+
+        ;; Start correction in changes
+        (add-hook 'after-change-functions #'languagetool-server--check-on-change nil 'local)
+        ;; Start correction on hooks
+        (dolist (hook (reverse languagetool-server-delayed-commands))
+          (add-hook hook #'languagetool-server-check nil 'local)))
+
+    (progn
+      ;; Delete active server checking timer if still active
+      (when languagetool-server--start-check-timer
+        (cancel-timer languagetool-server--start-check-timer))
+
+      ;; Delete all the checking hooks
+      (dolist (hook languagetool-server-delayed-commands)
+        (remove-hook hook #'languagetool-server-check 'local))
+      ;; Delete correction in changes
+      (remove-hook 'after-change-functions #'languagetool-server--check-on-change 'local)
+
+      ;; Delete all LanguageTool overlays
+      (languagetool-server--clear-buffer))))
+
+
+;; Server Checking Functions
+
+
+(defun languagetool-server--start-check ()
+  "Start checking for correct init of LanguageTool Server."
+  (let ((buffer (get-buffer-create languagetool-server-output-buffer-name)))
+    (with-current-buffer buffer
+      (save-excursion
+        (goto-char (point-min))
+        (when (or languagetool-server--started-p
+                  (search-forward "Server started" nil t))
+          (setq languagetool-server--started-p t)
+          (when languagetool-server--start-check-timer
+            (cancel-timer languagetool-server--start-check-timer))))))
+  (languagetool-server-check))
+
+(defun languagetool-server--check-on-change (_begin _end _len)
+  "Correct the buffer using LanguageTool and show its suggestion.
+
+This function checks for the actual showed region of the buffer
+for suggestions.  This function is an alias to
+`languagetool-server-check'.
+
+_BEGIN, _END and _LEN are unused as the buffer needs to be check
+completely."
+  (languagetool-server-check))
+
+(defun languagetool-server-check ()
+  "Correct the buffer using LanguageTool and show its suggestion.
+
+This function checks for the actual showed region of the buffer
+for suggestions."
+  (unless languagetool-server--correcting-p
+    (request
+      (format "%s:%d/v2/check" languagetool-server-url languagetool-server-port)
+      :type "POST"
+      :data (languagetool-server--parse-args)
+      :parser 'json-read
+      :success (cl-function
+                (lambda (&key response &allow-other-keys)
+                  (languagetool-server--show-corrections (request-response-data response))))
+      :error (cl-function
+              (lambda (&rest args &key error-thrown &allow-other-keys)
+                (languagetool-server-mode -1)
+                (message "LanguageTool got error: %S" error-thrown))))))
+
+(defun languagetool-server--parse-args ()
+  "Return the server argument list.
+
+Return the arguments as an assoc list of string which will be
+used in the POST request made to the LanguageTool server."
+  (let ((arguments (json-new-object)))
+
+    ;; Appends the correction language information
+    (setq arguments (json-add-to-object arguments "language" languagetool-default-language))
+
+    ;; Appends the mother tongue information
+    (when (stringp languagetool-mother-tongue)
+      (setq arguments (json-add-to-object arguments "motherTongue" languagetool-mother-tongue)))
+
+    ;; Appends the disabled rules
+    (let ((rules ""))
+      ;; Global disabled rules
+      (dolist (rule languagetool-disabled-rules)
+        (if (string= rules "")
+            (setq rules (concat rules rule))
+          (setq rules (concat rules "," rule))))
+      ;; Local disabled rules
+      (dolist (rule languagetool-local-disabled-rules)
+        (if (string= rules "")
+            (setq rules (concat rules rule))
+          (setq rules (concat rules "," rule))))
+      (unless (string= rules "")
+        (setq arguments (json-add-to-object arguments "disabledRules" rules))))
+
+    ;; Add the buffer contents
+    (setq arguments (json-add-to-object arguments "text"
+                                        (buffer-substring-no-properties (point-min) (point-max))))
+    arguments))
+
+(defun languagetool-server--show-corrections (json-parsed)
+  "Show the suggestions made by LanguageTool in the buffer.
+
+JSON-PARSED is a json object with the suggestions thrown by the
+LanguageTool Server."
+  (languagetool-server--clear-buffer)
+  (let ((corrections (cdr (assoc 'matches json-parsed)))
+        (correction nil))
+    (dotimes (index (length corrections))
+      (setq correction (aref corrections index))
+      (let ((offset (cdr (assoc 'offset correction)))
+            (size (cdr (assoc 'length correction))))
+        (languagetool--create-overlay
+         (+ (point-min) offset) (+ (point-min) offset size)
+         correction)))))
+
+(defun languagetool-server--clear-buffer ()
+  "Deletes all the buffer overlays."
+  (save-restriction
+    (widen)
+    (save-excursion
+      (dolist (ov (overlays-in (point-min) (point-max)))
+        (when (overlay-get ov 'languagetool-message)
+          (delete-overlay ov))))))
+
+
 
 ;; Create Functions:
 
